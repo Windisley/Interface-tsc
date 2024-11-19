9a15acc7ec2b1de40da7e16a4e60f0bf04d5d4e4
Windisley <windisleyl@gmail.com>
2024-11-19 19:36:24 -0300

    Interfaces Typescript

---
+++ build/dados.js
@@ -0,0 +1,7 @@
+"use strict";
+let data = {
+    id: "1",
+    Name: "João",
+    year: 20,
+    life: true
+};
---
+++ src/dados.ts
@@ -0,0 +1,16 @@
+interface DadosPerson{
+    id: string,
+    Name: string,
+    year: number,
+    life: boolean, 
+
+}
+
+
+let data:DadosPerson = {
+    id: "1",
+    Name: "João",
+    year: 20,
+    life: true
+
+}
